4c323fd9888d254bd1f0eb9badf56318fa3a4a16
ppsari <poppymighty@gmail.com>
2017-05-19 11:34:44 +0700

    search tampilkan yg available saja

--- routes/search.js
+++ routes/search.js
@@ -9,21 +9,42 @@ router.get('/', function(req, res, next) {
   let sport = req.query.sport;
 
   db.Venue.findAll({
-      where: { location: location }, include: {model: db.Sport }
+      where: { location: location }, include: {model: db.Sport, include:{model:db.SportDetail} }
     })
     .then((venues) => {
+
       venues = venues.filter((venue) => {
         let allsport = [];
         let idx =-1;
+        //futsal,basket, golf,kasti
         venue.Sports.forEach((sp, i) => {
           allsport.push(sp.name);
-          if (sp.name === sport) idx = i;
+
+          if (sp.name === sport){
+            // res.send(sp.SportDetails);
+            //futsal per lapangan
+            let idx2 = -1;
+            sp.SportDetails.forEach((dt)=>{
+              if (dt.avail !== '') idx2 = 0;
+            });
+
+            if (idx2 !== -1) {
+              idx = i;
+            }
+          }
         });
+        //end sport
+
         allsport = allsport.filter((v, i, a) => a.indexOf(v) === i).join(', ');
         venue.allsport = allsport;
         if (idx !== -1) return venue;
       });
+
+      // console.log('venue : ');
+      // res.send(venues)
       res.render('venueList',{venues,sport_id:sport});
+
+
     })
     .catch((err) => {
       res.send('Location invalid!');
